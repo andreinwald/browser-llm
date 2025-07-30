74b3433f26196e3eaf430f5beb786022d4c64c35
Andrii Reinvald <andreinwald@gmail.com>
2025-07-30 16:31:29 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
 import {useState, useEffect} from "react";
-import type {ChatCompletionMessageParam, MLCEngineInterface} from "@mlc-ai/web-llm";
+import {chat, loadLibrary, loadModel} from "./LLM.ts";
+import type {ChatCompletionMessageParam} from "@mlc-ai/web-llm/lib/openai_api_protocols/chat_completion";
 
 export function App() {
     const [status, setStatus] = useState<string | null>('loading LLM library');
+    const [response, setResponse] = useState<string>('');
 
     useEffect(() => {
         (async () => {
-            const llm = await loadLLM();
-            setStatus('running first prompt')
+            await loadLibrary();
+            setStatus('loading model');
+            // List of all models https://mlc.ai/models
+            await loadModel('Llama-3.2-1B-Instruct-q4f16_1-MLC', console.log);
+            setStatus('done');
             const messages: ChatCompletionMessageParam[] = [
                 {role: "user", content: "Why sky is blue?"},
             ]
-
-            const reply = await llm.chat.completions.create({
-                messages,
-            });
-            setStatus('done')
-            console.log(reply.choices[0].message);
+            const stream = await chat(messages);
+            let full = '';
+            for await (const chunk of stream) {
+                const delta = chunk?.choices?.[0]?.delta?.content ?? "";
+                if (delta) {
+                    full += delta;
+                    setResponse(full);
+                }
+            }
         })();
     }, []);
 
     return (
-        <>{status}</>
+        <>{status}
+            <br/>
+            <br/>
+            {response}
+        </>
     )
 }
 
 // if (!("gpu" in navigator)) {
 //     console.warn("WebGPU not available; WebLLM will try WASM fallback (much slower).");
 // }
-
-async function loadLLM(): Promise<MLCEngineInterface> {
-    return (await import('./LLM.ts')).default;
-}
-
--- src/LLM.ts
+++ src/LLM.ts
@@ -1,16 +1,34 @@
-import {CreateMLCEngine, type MLCEngineInterface} from "@mlc-ai/web-llm";
+import type {MLCEngineInterface} from "@mlc-ai/web-llm";
+import type {ChatCompletionMessageParam} from "@mlc-ai/web-llm/lib/openai_api_protocols/chat_completion";
 
-// List of all models https://mlc.ai/models
-const MODEL_ID = "Llama-3.2-1B-Instruct-q4f16_1-MLC";
-let enginePromise: Promise<MLCEngineInterface> | null = null;
+let createFunction: any = null;
 
-function getLLM(progressCallback?: (s: string) => void): Promise<MLCEngineInterface> {
-    enginePromise = CreateMLCEngine(
-        MODEL_ID,
-        {initProgressCallback: p => progressCallback && progressCallback(p?.text ?? p)}
-    );
-    enginePromise.then(() => console.log('loaded'));
-    return enginePromise;
+export async function loadLibrary() {
+    const {CreateMLCEngine} = await import("@mlc-ai/web-llm");
+    createFunction = CreateMLCEngine;
 }
 
-export default getLLM();
+let model: MLCEngineInterface | null = null;
+
+export async function loadModel(modelId: string, progressCallback?: (s: string) => void) {
+    if (!createFunction) {
+        throw new Error("Library not loaded");
+    }
+    model = await createFunction(
+        modelId,
+        {initProgressCallback: (p: any) => progressCallback && progressCallback(p?.text ?? p)}
+    );
+}
+
+export async function chat(messagesHistory: ChatCompletionMessageParam[]) {
+    if (!model) {
+        throw new Error("Model not loaded");
+    }
+
+    return model.chat.completions.create({
+        messages: messagesHistory,
+        stream: true,
+        max_tokens: 256,
+    });
+
+}
