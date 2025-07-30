c2e40b74f90b48da1cadf8cee7b3566123f7aada
Andrii Reinvald <andreinwald@gmail.com>
2025-07-30 11:05:11 +0200

    Added LLM

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import {render} from 'preact'
-import {App} from './app.tsx'
+import {App} from '././App.tsx'
 
+if (!("gpu" in navigator)) {
+    console.warn("WebGPU not available; WebLLM will try WASM fallback (much slower).");
+}
 
 render(App(), document.getElementById('app')!)
+setTimeout(() => {
+    import('./LLM.ts');
+}, 100);
