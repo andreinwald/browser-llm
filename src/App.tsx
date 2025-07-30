47523f249e235e2249c1430120561bddd34d1014
Andrii Reinvald <andreinwald@gmail.com>
2025-07-30 14:18:33 +0200

    working llm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
+import {useState, useEffect} from "react";
+import type {ChatCompletionMessageParam, MLCEngineInterface} from "@mlc-ai/web-llm";
 
 export function App() {
-  return (
-    <>works</>
-  )
+    const [status, setStatus] = useState<string | null>('loading LLM library');
+
+    useEffect(() => {
+        (async () => {
+            const llm = await loadLLM();
+            setStatus('running first prompt')
+            const messages: ChatCompletionMessageParam[] = [
+                {role: "user", content: "Why sky is blue?"},
+            ]
+
+            const reply = await llm.chat.completions.create({
+                messages,
+            });
+            setStatus('done')
+            console.log(reply.choices[0].message);
+        })();
+    }, []);
+
+    return (
+        <>{status}</>
+    )
 }
+
+// if (!("gpu" in navigator)) {
+//     console.warn("WebGPU not available; WebLLM will try WASM fallback (much slower).");
+// }
+
+async function loadLLM(): Promise<MLCEngineInterface> {
+    return (await import('./LLM.ts')).default;
+}
+
--- src/LLM.ts
+++ src/LLM.ts
@@ -1,14 +1,10 @@
 import {CreateMLCEngine, type MLCEngineInterface} from "@mlc-ai/web-llm";
-import type {ChatCompletionMessageParam} from "@mlc-ai/web-llm/lib/openai_api_protocols/chat_completion";
 
 // List of all models https://mlc.ai/models
 const MODEL_ID = "Llama-3.2-1B-Instruct-q4f16_1-MLC";
 let enginePromise: Promise<MLCEngineInterface> | null = null;
 
-export function getLLM(progressCallback?: (s: string) => void): Promise<MLCEngineInterface> {
-    if (enginePromise) {
-        return enginePromise;
-    }
+function getLLM(progressCallback?: (s: string) => void): Promise<MLCEngineInterface> {
     enginePromise = CreateMLCEngine(
         MODEL_ID,
         {initProgressCallback: p => progressCallback && progressCallback(p?.text ?? p)}
@@ -17,20 +13,4 @@ export function getLLM(progressCallback?: (s: string) => void): Promise<MLCEngin
     return enginePromise;
 }
 
-
-export async function askQuestion() {
-    const llm = await getLLM(console.log);
-    console.log('here2');
-
-    const messages: ChatCompletionMessageParam[] = [
-        {role: "user", content: "Why sky is blue?"},
-    ]
-
-    const reply = await llm.chat.completions.create({
-        messages,
-    });
-    console.log(reply.choices[0].message);
-    console.log(reply.usage);
-
-}
-askQuestion();
+export default getLLM();
